4d0208684a323cd1d5da0855ce503e2eed4f95dc
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-14 19:35:34 +0530

    added a card containing dropdowns fr stste and cities

--- src/components/Searchcard/SearchCard.jsx
+++ src/components/Searchcard/SearchCard.jsx
@@ -3,8 +3,16 @@ import Button from '../Button/Button';
 import DropDown from '../Dropdown/Dropdown';
 import { getStates, getCities , getCenters } from '../../api/api'; 
 import Search from '../../assets/Search.svg';
+import ServiceCard from '../ServicesCard/ServicesCard';
+import Doctor from '../../assets/Doctor.svg';
+import DrugStore from '../../assets/Drugstore.svg';
+import Hospital from '../../assets/Hospital.svg';
+import Capsule from '../../assets/Capsule.svg';
+import Ambulance from '../../assets/Ambulance.svg';
 import './SearchCard.css';
 
+
+const assests = [Doctor,DrugStore,Hospital,Capsule,Ambulance];
 
 function Searchcard({setCenters}){
     const [selectedState , setSelectedState] = useState('');
@@ -25,7 +33,7 @@ function Searchcard({setCenters}){
         fetchCenters();
     } , [selectedCity])
     return(
-        <div>
+        <div className='searchCard'>
             <div className='seachOptions'>
                 <DropDown placeholder='states' fetchData={getStates} setFunction={setSelectedState}/>
                 <DropDown placeholder='cities' fetchData={getCities} state={selectedState} setFunction={setSelectedCity}/>
@@ -33,6 +41,12 @@ function Searchcard({setCenters}){
                     <img src={Search}/>
                     Search
                 </Button>
+            </div>
+            <div className='serviceCards'>
+                <p className='helptext'>You may be looking for</p>
+                <div className='services'>
+                    {assests.map( (item) => <ServiceCard image={item}/>)}
+                </div>
             </div>
         </div>
     )
