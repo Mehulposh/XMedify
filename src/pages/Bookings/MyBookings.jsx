dab12c250a604330205ab1f7f8c830500bf1a74f
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-19 14:48:41 +0530

    added function to mubookings page

---
+++ src/pages/Bookings/MyBookings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MyBookings() {
+  return (
+    <div>
+      <h1>My Bookings</h1>
+      <p>This is the My Bookings page.</p>
+    </div>
+  );
+}   
+
+export default MyBookings;
